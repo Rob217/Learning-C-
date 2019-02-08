4d14c013034494b50d253a811115f1ed54c6901d
Rob217 <rjbettles89@gmail.com>
2019-02-08 21:19:53 +0100

    Write binary tree data structure

---
+++ DataStructures/BinaryTree.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+struct Node 
+{
+    int val;
+    Node *small = NULL;
+    Node *big = NULL;
+};
+
+class BinaryTree
+{
+    Node *head;
+public:
+    BinaryTree(): head {NULL}
+    {}
+
+    void Add(int val)
+    {
+        Node *temp = new Node;
+        temp->val = val;
+        if (head == NULL)
+        {
+            head = temp;
+            return;
+        }
+
+        Node *current = head;
+        while(true)
+        {
+            if (val < current->val) // go left
+            {
+                if (current->small == NULL) // assign new leaf node here
+                {
+                    current->small = temp;
+                    break;
+                }
+                else
+                    current = current->small; // move to next node to the left
+            }
+            else // go right
+            {
+                if (current->big == NULL) // assign new leaf node here
+                {
+                    current->big = temp;
+                    break;
+                }
+                else
+                    current = current->big; // move to next node to the right
+            }
+        }
+    }
+
+    void InOrderTraversal(Node *current)
+    {
+        if (current == NULL)
+            current = head;
+        // check left node
+        if (current->small)
+            InOrderTraversal(current->small);
+        // print current node
+        std::cout << current->val << "\n";
+        // check right node
+        if (current->big)
+            InOrderTraversal(current->big);
+    }
+
+
+};
+
+
+int main()
+{
+    BinaryTree b;
+    //
+    //              2
+    //            /   \
+    //          1       4
+    //                /   \
+    //              3       5
+    //
+    // In order: 1 - 2 - 3 - 4 - 5
+    // Pre order: 2 - 1 - 4 - 3 - 5
+    //
+    b.Add(2);
+    b.Add(4);
+    b.Add(1);
+    b.Add(5);
+    b.Add(3);
+    b.InOrderTraversal(NULL);
+    return 0;
+}
